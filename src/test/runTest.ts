8aa6f84a109748e4b1b48dfb8102bdefb6215b8d
James Cuzella <james.cuzella@lyraphase.com>
2025-06-04 13:02:33 -0600

    src/test/runTest.ts: Switch to 4 space indents (whitespace only change)

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -3,34 +3,34 @@ import * as path from 'path';
 import { runTests } from '@vscode/test-electron';
 
 async function main() {
-	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+    try {
+        // The folder containing the Extension Manifest package.json
+        // Passed to `--extensionDevelopmentPath`
+        const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
-		// The path to test runner
-		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, './suite/index');
+        // The path to test runner
+        // Passed to --extensionTestsPath
+        const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
-		console.log('Starting tests...');
-		console.log('Extension path:', extensionDevelopmentPath);
-		console.log('Test path:', extensionTestsPath);
+        console.log('Starting tests...');
+        console.log('Extension path:', extensionDevelopmentPath);
+        console.log('Test path:', extensionTestsPath);
 
-		// Download VS Code, unzip it and run the integration test
-		await runTests({
-			extensionDevelopmentPath, extensionTestsPath,
-			// launchArgs: ['--disable-extensions'],
-			// launchArgs: ['--enable-proposed-api nativeWindowHandle'],
-			// extensionTestsEnv: {
-			// 	VSCODE_DEBUG_MODE: 'true',
-			// 	VSCODE_EXTENSION_TESTS: 'true'
-			// }
-		 });
-	} catch (err) {
-		console.error('Failed to run tests');
-		console.error('Test run failed with error:', err);
-		process.exit(1);
-	}
+        // Download VS Code, unzip it and run the integration test
+        await runTests({
+            extensionDevelopmentPath, extensionTestsPath,
+            // launchArgs: ['--disable-extensions'],
+            // launchArgs: ['--enable-proposed-api nativeWindowHandle'],
+            // extensionTestsEnv: {
+            // 	VSCODE_DEBUG_MODE: 'true',
+            // 	VSCODE_EXTENSION_TESTS: 'true'
+            // }
+        });
+    } catch (err) {
+        console.error('Failed to run tests');
+        console.error('Test run failed with error:', err);
+        process.exit(1);
+    }
 }
 
 main();
