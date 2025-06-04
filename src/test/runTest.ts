84c76feb08784a6902848e66cd1202ac48acaafc
James Cuzella <james.cuzella@lyraphase.com>
2025-06-04 08:43:44 -0600

    ci: Add logging & env vars to @vscode/test-electron's runTests()

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -12,10 +12,23 @@ async function main() {
 		// Passed to --extensionTestsPath
 		const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
+		console.log('Starting tests...');
+		console.log('Extension path:', extensionDevelopmentPath);
+		console.log('Test path:', extensionTestsPath);
+
 		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
+		await runTests({
+			extensionDevelopmentPath, extensionTestsPath,
+			// launchArgs: ['--disable-extensions'],
+			// launchArgs: ['--enable-proposed-api nativeWindowHandle'],
+			extensionTestsEnv: {
+				VSCODE_DEBUG_MODE: 'true',
+				VSCODE_EXTENSION_TESTS: 'true'
+			}
+		 });
 	} catch (err) {
 		console.error('Failed to run tests');
+		console.error('Test run failed with error:', err);
 		process.exit(1);
 	}
 }
